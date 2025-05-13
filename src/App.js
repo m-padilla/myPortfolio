ad0df8c715d33e078e5cf54fb8eac5ceef9b1996
m-padilla <padillamarco18@gmail.com>
2025-05-13 11:33:20 -0700

    more of the initial file for portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom"
+import TopNaviBar from "./Components/Header/TopNaviBar";
+import About from './Components/Pages/About/About'
+// import Interests from './Components/Pages/Interests/Interests'
+// import Work from './Components/Pages/Work/Work'
+// import Projects from './Components/Pages/Projects/Projects'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="w-full h-screen bg-primart">
+      <TopNaviBar/>
+    <Routes>
+      <Route path="/about" element={<About/>} />
+      {/* <Route path="/interests" element={<Interests/>} />
+      <Route path="/work" element={<Work/>} />
+      <Route path="/projects" element={<Projects/>} /> */}
+    </Routes>
+  </div>  
   );
 }
 
